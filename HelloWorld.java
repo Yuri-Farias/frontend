b2c75e059d4a51d6d56f4ee4f2f5b70477675a14
Yuri 7 <yurifarias444@gmail.com>
2024-08-13 11:16:14 -0300

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -22,5 +22,5 @@ public class HelloWorld { // Aqui é criada a classe HelloWorld
      */
      public static void main (String[] args) { // Aqui é criado o método executor main
          System.out.println("Hello World!!!"); // Aqui é "impressa" uma linha com o texto "Hello World"
-     } //  Aqui encerra o bloco de codigo do método main
-}  // Aqui encerra o bloco de codigo da classe  HelloWorld
+     } //  Aqui encerra o bloco de código do método main
+}  // Aqui encerra o bloco de código da classe  HelloWorld
