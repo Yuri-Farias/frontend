404d7bc1b7b3049425220f251fe7a2ae12fdfcf8
Yuri 7 <yurifarias444@gmail.com>
2024-08-15 09:22:36 -0300

    tradução

--- Addition.java
+++ Addition.java
@@ -5,16 +5,16 @@ public class Addition // Aqui é criada a classe Addition
    public static void main(String[] args) // Aqui é criado o método executor main
    { // Aqui começa o bloco de código do método main      
       String firstNumber = // Aqui é o nome da variável da String
-         JOptionPane.showInputDialog("Enter first integer"); // Exibe um diálogo com uma caixa de dados pedindo um número inteiro
+         JOptionPane.showInputDialog("Insira o primeiro número inteiro"); // Exibe um diálogo com uma caixa de dados pedindo um número inteiro
       String secondNumber = // Aqui é o nome da segunda variável da String
-         JOptionPane.showInputDialog("Enter second integer"); // Exibe um diálogo com uma caixa de dados pedindo um segundo número inteiro
+         JOptionPane.showInputDialog("Insira o segundo número inteiro"); // Exibe um diálogo com uma caixa de dados pedindo um segundo número inteiro
       
       int number1 = Integer.parseInt(firstNumber); // Aqui o componente vai invocar um metodo (parse) de análise para a variável (firtNumber)
       int number2 = Integer.parseInt(secondNumber); // Aqui o componente vai invocar um metodo (parse) de análise para a variável (secondNumber)
 
       int sum = number1 + number2;  // Aqui é a soma/resultado dos números 1 e 2 (???)
 
-      JOptionPane.showMessageDialog(null, "The sum is " + sum, // Aqui mostra uma mensagem gerada por parâmetros "The sum is" que vem com a soma 
-         "Sum of Two Integers", JOptionPane.PLAIN_MESSAGE); // Aqui é a continuação dos parâmetros após a soma ser feita e acertada, vai aparecer a mensagem de confirmação "Sum Of Two Integers"
+      JOptionPane.showMessageDialog(null, "A soma é" + sum, // Aqui mostra uma mensagem gerada por parâmetros "The sum is" que vem com a soma 
+         "Soma de dois números inteiros", JOptionPane.PLAIN_MESSAGE); // Aqui é a continuação dos parâmetros após a soma ser feita e acertada, vai aparecer a mensagem de confirmação "Sum Of Two Integers"
    } // Aqui encerra o bloco de código do método main
 } // Aqui encerra o bloco de código da classe Addition
